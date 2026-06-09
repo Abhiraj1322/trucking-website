e7478871abded5ac8392f2ab61b05e5787549fdf
abhiraj1322 <abhirajdogra2@gmail.com>
2026-06-08 23:08:14 -0400

    changes made

--- Components/Hero.jsx
+++ Components/Hero.jsx
@@ -13,8 +13,8 @@ const Hero = () => {
       <div className="absolute inset-0 bg-black/50" />
 
       <div className="relative text-center text-white">
-        <h1 className="text-3xl font-bold mb-4 animate-slide-in">
-   Reliable Freight Solutions Serving the GTA for 15+ Years
+        <h1 className="text-1xl md text-3xl  font-bold mb-4 animate-slide-in">
+Your Trusted GTA Freight Partner
         </h1>
 
         <p className="mb-6">
